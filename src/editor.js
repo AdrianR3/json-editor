1c9aa228fdd160702cdd2820a99570592961d87a
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2024-08-28 01:41:08 -0700

    Add scripts for Clear and Beautify

--- src/editor.js
+++ src/editor.js
@@ -1,8 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let editor = CodeMirror(document.getElementById('editor'), {
+  var editor = CodeMirror(document.getElementById('editor'), {
     mode: 'application/json',
     theme: 'dracula',
     lineNumbers: true,
     value: `{"hello": "world"}`
   });
+
+  console.log(editor.doc.getValue())
+
+  document.querySelectorAll('#nav > button.nav-button').forEach(button => button.addEventListener('click', (e) => {
+    // e.stopPropagation()
+    const button = e.currentTarget.getAttribute('name');
+    switch (button) {
+      case 'export': 
+        
+        // Not Implemented Yet
+  
+        break;
+      case 'beautify':
+
+        editor.doc.setValue(
+          JSON.stringify(JSON.parse(editor.doc.getValue()), null, 2)
+        );
+
+        break;
+      case 'minify':
+
+        // Not Implemented Yet
+
+        break;
+      case 'clear':
+        editor.doc.setValue('');
+        break;
+    }
+  }))
+
 });
